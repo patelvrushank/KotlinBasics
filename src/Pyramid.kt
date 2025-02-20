f196fa3d89a5c96121d257dbd90b90c52c71576c
vrushankpatel <patelvd99@gmail.com>
2025-02-20 15:00:17 -0500

    Pyramid program commit

---
+++ src/Pyramid.kt
@@ -0,0 +1,20 @@
+fun main(){
+
+    print(pyramid(5))
+}
+
+fun pyramid(n:Int):MutableList<String>{
+    val result = mutableListOf<String>()
+    val maxRow = n * 2 - 1
+
+    (1..n).forEach {
+       val  rowLen = it * 2 - 1
+
+       val sideString = ".".repeat((maxRow - rowLen) / 2)
+        val hashString = "#".repeat(rowLen)
+
+        result.add("$sideString$hashString$sideString")
+    }
+    return result
+
+}
